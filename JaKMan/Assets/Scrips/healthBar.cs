93cf12d1b73e0f5ce0a9190834873cabf864d071
s4msung2650 <jakobdahlk@hotmail.com>
2014-04-28 16:59:27 +0200

    scripts

---
+++ JaKMan/Assets/Scrips/healthBar.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthBar : MonoBehaviour {
+
+	// Use this for initialization
+	
+	public int Life = 1;
+    public Texture TextureHealth;
+	public SphereCollider Player;
+	
+	void Start () {	
+		Player = gameObject.GetComponent<SphereCollider>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		    if(Life==0){
+               Application.LoadLevel (0);
+			
+		}
+	}
+			
+	void OnGUI(){
+	    if ( Life!=0){
+	            for (int i=0; i<Life; i++){
+	                  int posX = 50+50*i;
+	                  GUI.DrawTexture(new Rect(posX,30,30,30),TextureHealth);
+				}
+	
+		}
+	}
+	
+}
+		
+
---
+++ JaKMan/Assets/Scrips/score.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score : MonoBehaviour
+{
+	
+	double Counter = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		Counter +=25;
+	
+	}
+	void OnGUI()
+	{	
+	
+		guiText.text = "Score: "+Counter; 
+			
+	}
+}
