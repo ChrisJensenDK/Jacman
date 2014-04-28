1fc384098aa3ccb6e118b7df8e56bc0b2887fac3
Christian Sørup Jensen <chrjen12@student.aau.dk>
2014-04-28 17:02:55 +0200

    Upload of drowning and move scripts

---
+++ JaKMan/Assets/Scrips/drowning.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drowning : MonoBehaviour {
+
+	public Transform exp; // enabling an "explosion" (the splash effect)
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.name == "JakMan") {
+			Instantiate (exp, transform.position, Quaternion.identity);
+			Destroy (other.gameObject);
+		}
+
+	}
+}
