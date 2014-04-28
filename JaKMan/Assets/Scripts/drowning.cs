3fcc9f626c766bd406b52f1664073ee630f29e18
s4msung2650 <jakobdahlk@hotmail.com>
2014-04-28 17:59:40 +0200

    merge of drowning and healthBar, still not completed

--- JaKMan/Assets/Scripts/drowning.cs
+++ JaKMan/Assets/Scripts/drowning.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class drowning : MonoBehaviour {
 
 	public Transform exp; // enabling an "explosion" (the splash effect)
+	public Texture TextureHealth;
+	public int Life = 1;
+	
 
 	// Use this for initialization
 	void Start () {
@@ -12,14 +15,26 @@ public class drowning : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(Life==0){
+          Application.LoadLevel (0);
+		}
 	
 	}
 
 	void OnCollisionEnter(Collision other){
 		if (other.gameObject.name == "JakMan") {
 			Instantiate (exp, transform.position, Quaternion.identity);
-			Destroy (other.gameObject);
+			Life--;
 		}
-
+	}
+	
+		void OnGUI(){
+	    if ( Life!=0){
+	            for (int i=0; i<Life; i++){
+	                  int posX = 50+50*i;
+	                  GUI.DrawTexture(new Rect(posX,30,30,30),TextureHealth);
+				}
+	
+		}
 	}
 }
