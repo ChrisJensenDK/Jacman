4926e9fcfc12c6aca40311f7f82a80d67c39fe54
Christian Sørup Jensen <chrjen12@student.aau.dk>
2014-04-29 11:31:10 +0200

    Added the enemy script again

---
+++ JaKMan/Assets/Scripts/enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+	
+	public Transform exp; // enabling an explosion
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var speed: float = 3;
+		var rotRange: float = 45; // random angle will be between -rotRange and rotRange
+		var timeToChange: float = 5; // change direction each timeToChange seconds
+		
+		private var timer: float = 0; // start change timer
+		
+		function Update(){
+			var controller : CharacterController = GetComponent(CharacterController);
+			timer = timer - Time.deltaTime; // decrements timer counter
+			if (timer
+	}
+	
+
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.name == "JakMan"){
+			Instantiate (exp, transform.position, Quaternion.identity);
+			Destroy (other.gameObject);
+		}
+	}
+}
