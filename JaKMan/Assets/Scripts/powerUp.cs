75c1e06e623345410460aa602b80a8c5758d6d60
s4msung2650 <jakobdahlk@hotmail.com>
2014-04-29 18:10:37 +0200

    update

---
+++ JaKMan/Assets/Scripts/powerUp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class powerUp : MonoBehaviour {
+	healthBar health;
+
+	public Transform exp; 
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, 2.0f);
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.name == "JakMan") {
+			Destroy (this.gameObject);
+			Instantiate (exp, transform.position, Quaternion.identity);
+			health = other.gameObject.GetComponent<healthBar>();
+			health.Life++;
+			other.transform.position = health.startPos;
+
+		}
+	}
+
+}
