7ed0df8614986f862852efd4913b2a5baa158a01
Christian Sørup Jensen <chrjen12@student.aau.dk>
2014-04-29 15:27:54 +0200

    Added rotation to the coins, as well as collision with Jacman. Issue: enemy cannot move through coins

---
+++ JaKMan/Assets/Scripts/coin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coin : MonoBehaviour {
+
+	public Transform exp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, 2.0f);
+	}
+
+	void OnCollisionEnter (Collision other){
+		if(other.gameObject.name == "JakMan") {
+			Destroy(this.gameObject);
+		}
+	}
+}
