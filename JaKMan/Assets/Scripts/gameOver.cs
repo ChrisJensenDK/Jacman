01f1d5235dedd97081aa1355dffa63d06c12a154
s4msung2650 <jakobdahlk@hotmail.com>
2014-04-30 04:16:10 +0200

    small updates

--- JaKMan/Assets/Scripts/gameOver.cs
+++ JaKMan/Assets/Scripts/gameOver.cs
@@ -5,15 +5,18 @@ public class gameOver : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	void OnGUI () {
+			//guiText.text = "Score: "+highScore; 
+			
+		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
 		if(GUI.Button(new Rect(412,350,200,100), "Return To Main Menu")) {
 			Application.LoadLevel(0);
--- JaKMan/Assets/Scripts/score.cs
+++ JaKMan/Assets/Scripts/score.cs
@@ -5,20 +5,18 @@ using System.Collections;
 
 public class score : MonoBehaviour
 {
-	double Counter = 50000;
+	double counter = 50000;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		
-		Counter -=5;
-
-		if(Counter==0){
+		counter -=5;
+		if(counter==0){
 			Application.LoadLevel (2);
 		}
 	
@@ -26,7 +24,7 @@ public class score : MonoBehaviour
 	void OnGUI()
 	{	
 	
-		guiText.text = "Score: "+Counter; 
+		guiText.text = "Score: "+counter; 
 			
 	}
 }
